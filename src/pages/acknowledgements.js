9283840bbbdd52c4901c9740b22eba01fc475448
Tony D'Addeo <tony@askhr.com>
2020-03-06 11:34:49 -0600

    t

--- src/pages/acknowledgements.js
+++ src/pages/acknowledgements.js
@@ -1,33 +1,47 @@
 import React from "react"
 import Layout from "../components/layout"
 
-export default () =>
+export default () => (
   <Layout>
     <h1>Acknowledgements</h1>
 
     <p>
-      Almost everything you see on this website was adapted from someone else. Usually
-      the someone else is someone I don't know personally. Thank you people!
+      Almost everything you see on this website was adapted from someone else.
+      Usually the someone else is someone I don't know personally. Thank you
+      people!
     </p>
 
     <ul>
-      <li className="pb-1">
-        <a href="https://www.perell.com/">David Perrel</a> for his posts and guides on writing online.
+      <li>
+        <a href="https://www.instinctbodywork.org/">Olga Vilkotskaya</a> for her
+        keen eye and invigorating support.
       </li>
-      <li className="pb-1">
-        <a href="https://sivers.org/">Derek Sivers</a> for his "now page" and stellar personal site.
+      <li>
+        <a href="https://www.perell.com/">David Perrel</a> for his work on
+        writing online. It was some tweet of his that encouraged me to build
+        this site.
       </li>
-      <li className="pb-1">
-        <a href="https://tomcritchlow.com/">Tom Critchlow</a> for his collapsible outline approach to organizing content.
+      <li>
+        <a href="https://sivers.org/">Derek Sivers</a> for his highly original
+        personal site and his "now page" concept. If authenticity is the new
+        cool than Derek's the new Paul Newman.
       </li>
-      <li className="pb-1">
-        <a href="https://davidklaing.com/">David Klaing</a> for his home page which served as great example in many ways.
+      <li>
+        <a href="https://tomcritchlow.com/">Tom Critchlow</a> for his
+        collapsible outline approach to organizing content.
       </li>
-      <li className="pb-1">
-        <a href="https://www.ribbonfarm.com/about/">Venkatesh Rao</a> for his domestic cozy concept, a fun aesthetic to design to.
+      <li>
+        <a href="https://davidklaing.com/">David Klaing</a> for his simple and
+        effective home page which served as a great example in many ways.
       </li>
-      <li className="pb-1">
-        <a href="https://simonsarris.com/">Simon Sarris</a> for his highly aesthetic Simeville.
+      <li>
+        <a href="https://www.ribbonfarm.com/about/">Venkatesh Rao</a> for his
+        domestic cozy concept, a fun aesthetic to design to.
+      </li>
+      <li>
+        <a href="https://simonsarris.com/">Simon Sarris</a> for his highly
+        aesthetic Simeville.
       </li>
     </ul>
   </Layout>
+)
