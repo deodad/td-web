13785d61573c6089b0e9b0ab049c1b0705922336
Tony D'Addeo <tony@askhr.com>
2020-03-10 12:38:22 -0500

    Add analytics
    I would love to not use Google Analytics but for now it's
    meeting my budget and requirements.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,53 @@
-const path = require('path')
+const path = require("path")
 
 module.exports = {
   siteMetadata: {
     title: "Tony D'Addeo",
     description: "Tony's homepage",
-    author: '@adaddeo',
+    author: "@adaddeo",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
-        path: path.join(__dirname, 'src', 'images')
-      }
+        name: "images",
+        path: path.join(__dirname, "src", "images"),
+      },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'posts',
-        path: path.join(__dirname, 'src', 'posts')
-      }
+        name: "posts",
+        path: path.join(__dirname, "src", "posts"),
+      },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-classes',
+            resolve: "gatsby-remark-classes",
             options: {
               classMap: {
-                'list[ordered=true]': 'list-decimal list-inside',
-                'list[ordered=false]': 'list-disc list-inside',
-                'listItem': 'mb-3',
-              }
-            }
+                "list[ordered=true]": "list-decimal list-inside",
+                "list[ordered=false]": "list-disc list-inside",
+                listItem: "mb-3",
+              },
+            },
           },
-          'gatsby-plugin-catch-links'
-        ]
-      }
+          "gatsby-plugin-catch-links",
+        ],
+      },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-postcss'
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-153501057-1",
+      },
+    },
   ],
 }
