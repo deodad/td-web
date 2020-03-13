b1b34eb4fe5a980337bbd7623eaa7851b3b6ef70
Tony D'Addeo <tony@askhr.com>
2020-03-13 09:51:18 -0500

    Improve link accessibility

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -3,6 +3,6 @@ import { Link } from "gatsby"
 import classnames from "classnames"
 
 export default ({ to, className, children }) =>
-  <Link to={to} className={classnames('block', 'visited:text-blue-600', className)}>
+  <Link to={to} className={classnames('block', 'visited:text-blue-700', className)}>
     {children}
   </Link>
